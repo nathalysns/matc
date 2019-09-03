716e2bee64507edce3cdb580446b75d1e07554ae
nathaly <ssanties@jlab.org>
2019-09-03 11:24:32 -0400

    modified:   s2.txt

---
+++ detectors/cer.C
@@ -0,0 +1,59 @@
+#include "../rootalias_spring2018.h"
+#include "../HallA_style.h"
+
+TString arm;
+TString armHRS;
+TChain *T;
+TChain *E;
+
+
+void cer(Int_t run){
+
+    HallA_style();
+
+    TH1F *cer = new TH1F("cer","",100,150,600);
+    Int_t N = 101;
+
+    //=================================================//
+    TCut totalcut = sh_cut_L + cer_cut_L + track_L;
+    TCut trigger = trigger_L;
+    TCut trigger1 = trigger1_L;
+    TCut trigger3 = trigger3_L;
+
+    TString arm = "L";
+
+    if(run>90000){ totalcut = sh_cut_R + cer_cut_R + track_R; arm = "R";
+    trigger = trigger_R;
+    trigger1 = trigger4_R;
+    trigger3 = trigger6_R;
+    }
+
+    cout << run << endl;
+
+    //run=3102;
+    T = LoadRun(run,"T");
+    ofstream outfile;
+  	outfile.open ("cer.txt",ios::in|ios::app);
+  	outfile << setiosflags(ios::left) << setw(8) << run;
+	for(Int_t i=0; i<10;i++){
+
+    	T = LoadRun(run,"T");
+    	T->Draw(Form("%s.cer.a_c[%i]>>cer",i),arm, trigger_L,"goff");
+      Double_t min = 240;
+      Double_t max = 350;
+      
+
+      cer->Fit("gaus","Q","",min,max);
+   		TF1 *myfunc=cer->GetFunction("gaus");
+   		cout << "Peak  = " <<  myfunc->GetParameter(1) << " +/- " << myfunc->GetParameter(2) << endl;
+
+   		outfile << setiosflags(ios::left) << setw(15) << myfunc->GetParameter(1);
+  		outfile << setiosflags(ios::left) << setw(15) << myfunc->GetParameter(2);
+	}
+
+  outfile << endl;
+
+	outfile.close();
+
+ 
+}
