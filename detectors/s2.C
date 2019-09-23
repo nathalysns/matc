75e87632acfea5788a03b2f1de83f6707228ec43
Nathaly Santiesteban <nathaly@nathalys-mbp.aw4.unh.edu>
2019-09-23 12:23:05 -0400

    modified:   detectors/s2.C 	modified:   runs_list.csv

--- detectors/s2.C
+++ detectors/s2.C
@@ -32,13 +32,21 @@ void s2(Int_t run){
     T = LoadRun(run,"T");
     ofstream outfile;
   	outfile.open ("s2.txt",ios::in|ios::app);
+
+    TCanvas *c1 = new TCanvas("c1","",1500, 1500);
+    c1->Divide(4,4);
+
+    TCanvas *c2 = new TCanvas("c2","",1500, 1500);
+    c2->Divide(4,4);
+
   	
     for(Int_t i=0; i<16;i++){
+
+        c1->cd(i+1);
     	T->Draw(Form("%s.s2.la_c[%i]>>s2l",arm.Data(),i),"","goff");
-    	//T->Draw(Form("%s.s2.ra_c[%i]>>s2r",arm.Data(),i),"","goff");
-         
-	Double_t len = 40;
-//	#if(i==0 || i==15) len =50;
+    	 
+    	Double_t len = 40;
+
     	Int_t max_binl = s2l->GetMaximumBin();
     	Double_t minl = s2l->GetBinCenter(max_binl) - len;
     	Double_t maxl = s2l->GetBinCenter(max_binl) + len + 80;
@@ -50,7 +58,7 @@ void s2(Int_t run){
     	TF1 *myfunc2l=s2l->GetFunction("landau");
     	cout<<"peak channel l: "<<myfunc2l->GetParameter(1) << "+/-" <<myfunc2l->GetParError(1) <<endl;
 
-/*
+        c2->cd(i+1);
     	Int_t max_binr = s2r->GetMaximumBin();
     	Double_t minr = s2r->GetBinCenter(max_binr) - len;
     	Double_t maxr = s2r->GetBinCenter(max_binr) + len;
@@ -62,9 +70,9 @@ void s2(Int_t run){
     	TF1 *myfunc2r=s2r->GetFunction("landau");
     	cout<<"peak channel r: "<<myfunc2r->GetParameter(1) << "+/-" <<myfunc2r->GetParError(1) <<endl;
 
-	Double_t t1 = myfunc2l->GetParameter(1);
-	Double_t t1er = myfunc2l->GetParError(1);
-	Double_t t2 = myfunc2r->GetParameter(1);
+	    Double_t t1 = myfunc2l->GetParameter(1);
+	    Double_t t1er = myfunc2l->GetParError(1);
+	    Double_t t2 = myfunc2r->GetParameter(1);
         Double_t t2er = myfunc2r->GetParError(1);
 
 	if (t1 <= 50) { t1 = 0; t1er =0;}
@@ -72,7 +80,7 @@ void s2(Int_t run){
     	
   		outfile << setiosflags(ios::left) << setw(8) << run;
   		outfile << setiosflags(ios::left) << setw(15) << t1;
-  		outfile << setiosflags(ios::left) << setw(15) << t1er;
+  		outfile << setiosflags(ios::left) << setw(15) << t1er;t1
   		outfile << setiosflags(ios::left) << setw(15) << t2;
   		outfile << setiosflags(ios::left) << setw(15) << t2er; 
   		
@@ -81,5 +89,5 @@ void s2(Int_t run){
 	outfile << endl;
 	outfile.close();
 
-*/
-}}
+
+}
