cffb7c5845ee5176391447963b6dc847c217526e
nathaly <ssanties@jlab.org>
2019-09-23 20:09:28 -0400

    modified:   s0.C 	modified:   s0.txt 	modified:   s2.C 	modified:   s2.txt

--- detectors/s2.C
+++ detectors/s2.C
@@ -17,18 +17,16 @@ void s2(Int_t run){
     TCut trigger1 = trigger1_L;
     TCut trigger3 = trigger3_L;
 
-    Int_t adc_ini =200;
+    Int_t adc_ini =180;
     TString arm = "L";
     if(run>90000){ totalcut = sh_cut_R + cer_cut_R + track_R; arm = "R";
     trigger = trigger_R;
     trigger1 = trigger4_R;
     trigger3 = trigger6_R;
-    adc_ini = 150;
+    adc_ini = 90;
     }
 
-    TH1F *s2l = new TH1F("s2l","",200,adc_ini,500);
-    TH1F *s2r = new TH1F("s2r","",200,adc_ini,500);
-    //run=3102;
+   //run=3102;
     T = LoadRun(run,"T");
     ofstream outfile;
   	outfile.open ("s2.txt",ios::in|ios::app);
@@ -39,35 +37,40 @@ void s2(Int_t run){
     TCanvas *c2 = new TCanvas("c2","",1500, 1500);
     c2->Divide(4,4);
 
+    TH1F *s2l[16];
+    TH1F *s2r[16];
   	
     for(Int_t i=0; i<16;i++){
-
+    
+ 	s2l[i] = new TH1F(Form("s2l%i",i),"",100,adc_ini,500);
+        s2r[i] = new TH1F(Form("s2r%i",i),"",100,adc_ini,500);
+ 
         c1->cd(i+1);
-    	T->Draw(Form("%s.s2.la_c[%i]>>s2l",arm.Data(),i),"","goff");
-    	 
-    	Double_t len = 40;
-
-    	Int_t max_binl = s2l->GetMaximumBin();
-    	Double_t minl = s2l->GetBinCenter(max_binl) - len;
-    	Double_t maxl = s2l->GetBinCenter(max_binl) + len + 80;
-    	s2l->Fit("landau","Q","",minl,maxl);
-    	TF1 *myfuncl=s2l->GetFunction("landau");
-    	minl = myfuncl->GetParameter(1) - 1.5*myfuncl->GetParameter(2);
-    	maxl = myfuncl->GetParameter(1) + 4*myfuncl->GetParError(2);
-    	s2l->Fit("landau","Q","",minl,maxl);
-    	TF1 *myfunc2l=s2l->GetFunction("landau");
+    	T->Draw(Form("%s.s2.la_c[%i]>>s2l%i",arm.Data(),i,i),"","");
+     
+    	Double_t len = 50;
+    	Int_t max_binl = s2l[i]->GetMaximumBin();
+    	Double_t minl = s2l[i]->GetBinCenter(max_binl) - len;
+    	Double_t maxl = s2l[i]->GetBinCenter(max_binl) + len;
+    	s2l[i]->Fit("landau","Q","",minl,maxl);
+    	TF1 *myfuncl=s2l[i]->GetFunction("landau");
+    	minl = myfuncl->GetParameter(1) - 1.7*myfuncl->GetParameter(2);
+    	maxl = myfuncl->GetParameter(1) + 3.*myfuncl->GetParameter(2);
+    	s2l[i]->Fit("landau","Q","",minl,maxl);
+    	TF1 *myfunc2l=s2l[i]->GetFunction("landau");
     	cout<<"peak channel l: "<<myfunc2l->GetParameter(1) << "+/-" <<myfunc2l->GetParError(1) <<endl;
 
         c2->cd(i+1);
-    	Int_t max_binr = s2r->GetMaximumBin();
-    	Double_t minr = s2r->GetBinCenter(max_binr) - len;
-    	Double_t maxr = s2r->GetBinCenter(max_binr) + len;
-    	s2r->Fit("landau","Q","",minr,maxr);
-    	TF1 *myfuncr=s2r->GetFunction("landau");
-    	minr = myfuncr->GetParameter(1) - 1.*myfuncr->GetParameter(2);
-    	maxr = myfuncr->GetParameter(1) + 1.*myfuncr->GetParameter(2);
-    	s2r->Fit("landau","Q","",minr,maxr);
-    	TF1 *myfunc2r=s2r->GetFunction("landau");
+    	T->Draw(Form("%s.s2.la_c[%i]>>s2r%i",arm.Data(),i,i),"","");
+    	Int_t max_binr = s2r[i]->GetMaximumBin();
+    	Double_t minr = s2r[i]->GetBinCenter(max_binr) - len;
+    	Double_t maxr = s2r[i]->GetBinCenter(max_binr) + len;
+    	s2r[i]->Fit("landau","Q","",minr,maxr);
+    	TF1 *myfuncr=s2r[i]->GetFunction("landau");
+    	minr = myfuncr->GetParameter(1) - 1.7*myfuncr->GetParameter(2);
+    	maxr = myfuncr->GetParameter(1) + 3.*myfuncr->GetParameter(2);
+    	s2r[i]->Fit("landau","Q","",minr,maxr);
+    	TF1 *myfunc2r=s2r[i]->GetFunction("landau");
     	cout<<"peak channel r: "<<myfunc2r->GetParameter(1) << "+/-" <<myfunc2r->GetParError(1) <<endl;
 
 	    Double_t t1 = myfunc2l->GetParameter(1);
@@ -80,7 +83,7 @@ void s2(Int_t run){
     	
   		outfile << setiosflags(ios::left) << setw(8) << run;
   		outfile << setiosflags(ios::left) << setw(15) << t1;
-  		outfile << setiosflags(ios::left) << setw(15) << t1er;t1
+  		outfile << setiosflags(ios::left) << setw(15) << t1er;
   		outfile << setiosflags(ios::left) << setw(15) << t2;
   		outfile << setiosflags(ios::left) << setw(15) << t2er; 
   		
@@ -88,6 +91,5 @@ void s2(Int_t run){
 
 	outfile << endl;
 	outfile.close();
-
 
 }
