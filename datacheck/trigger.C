de8ffdb8030e92d99578c1f1767250e89334a782
Nathaly Santiesteban <nathaly@Nathalys-MacBook-Pro.local>
2019-10-22 10:26:56 -0400

    modified:   trigger.C

--- datacheck/trigger.C
+++ datacheck/trigger.C
@@ -51,6 +51,10 @@ void trigger(Int_t run){
     TCut trig3 = "DL.bit3>0";
     TCut total =  dp_cut_L_loose + th_cut_L_loose + ph_cut_L_loose + track_L + sh_cut_L;
     TCut beta = "L.tr.beta>0.7 && L.tr.beta<1.5";
+    TCut shtest = "";
+    TString sht1 = "";
+    Double_t  m, a1, a2, c1, c2; 
+    m = a1 = a2 = c1 = c2 = 0; 
 
     if(run>90000){ 
         arm = "R";
@@ -59,6 +63,20 @@ void trigger(Int_t run){
         trig3 = "DR.bit6>0";
         total =  dp_cut_R_loose + th_cut_R_loose + ph_cut_R_loose + track_R + sh_cut_R;
         beta = "R.tr.beta>0.7 && R.tr.beta<1.5";
+        sht1 = "(R.sh.e+R.ps.e)>((%f*(R.tr.p[0]*1000))-%f) && (R.sh.e+R.ps.e)<((%f*(R.tr.p[0]*1000))-%f) && (R.tr.p[0]*1000)>%f &&(R.tr.p[0]*1000)<%f ";
+        //
+        if(run>94024 && run <94067){a1=1500; a2=900; c1=2810; c2=3120;} //R28-HS
+        else if(run>93986 && run <94015){a1=1500; a2=800; c1=2650; c2=3120;} //R28-PK
+        else if(run>93741 && run <93774){a1=1500; a2=800; c1=2800; c2=3400;} //R26-HS
+        else if(run>93708 && run <93739){a1=1500; a2=800; c1=2750; c2=3400;} //R26-PK
+        else if(run>93774 && run <93786){a1=1500; a2=800; c1=2700; c2=3200;} //R26-LS
+        else if(run>93591 && run <93644){a1=1600; a2=800; c1=2900; c2=3400;} //R24-PK
+        else if(run>93648 && run <93699){a1=1700; a2=800; c1=2700; c2=3300;} //R24-LS
+        else if(run>93092 && run <93124){a1=850; a2=250; c1=1320; c2=1550;} //R42-HS
+        else if(run>93044 && run <93092){a1=850; a2=100; c1=1200; c2=1550;} //R42-PK
+        else if(run>93125 && run <93149){a1=850; a2=100; c1=1200; c2=1550;} //R42-LS
+        else {a1=3000; a2=0; c1=1000; c2=4000;}
+        shtest = Form(sht1,m,a1,m,a2,c1,c2);
     }
 
 
@@ -67,16 +85,16 @@ void trigger(Int_t run){
     TH1F *h3 = new TH1F("h3","",500,0,2);
     TH1F *h4 = new TH1F("h4","",500,0,2);
 
-    T->Draw(Form("EK%sx.x_bj>>h1",arm.Data()), datacurrentcut + total + beta, "goff" );
+    T->Draw(Form("EK%sx.x_bj>>h1",arm.Data()), datacurrentcut + total + beta + shtest, "goff" );
     Double_t all = h1->GetEntries();
 
-    T->Draw(Form("EK%sx.x_bj>>h2",arm.Data()), datacurrentcut + total + trig1 + trig2 + trig3 + beta, "goff" );
+    T->Draw(Form("EK%sx.x_bj>>h2",arm.Data()), datacurrentcut + total + trig1 + trig2 + trig3 + beta + shtest, "goff" );
     Double_t alltrig = h2->GetEntries();
 
-    T->Draw(Form("EK%sx.x_bj>>h3",arm.Data()), datacurrentcut + total + trig1 + !trig2 + !trig3 + beta, "goff" );
+    T->Draw(Form("EK%sx.x_bj>>h3",arm.Data()), datacurrentcut + total + trig1 + !trig2 + !trig3 + beta + shtest, "goff" );
     Double_t onlytrig1 = h3->GetEntries();
 
-    T->Draw(Form("EK%sx.x_bj>>h4",arm.Data()), datacurrentcut + total + !trig1 + !trig2 + trig3 + beta, "goff" );
+    T->Draw(Form("EK%sx.x_bj>>h4",arm.Data()), datacurrentcut + total + !trig1 + !trig2 + trig3 + beta + shtest, "goff" );
     Double_t onlytrig3 = h4->GetEntries();
 
     ofstream outfile;
